967dbf8ae33b29a46ef256f98dd6466c354efa7b
KetanVerma <Ketan18710@gmail.com>
2019-01-21 16:32:02 +0530

    issues in onclick

---
+++ v0.0.0/portfolio/main.js
@@ -0,0 +1,24 @@
+ let sec1 = document.getElementById('sec1');
+ let sec2 = document.getElementById('sec2');
+ let sec3 = document.getElementById('sec3');
+ let sec4 = document.getElementById('sec4');
+ let sec = document.getElementsByClassName("content");
+ function change_display(j)
+ {
+    sec[j].style.display = 'flex';
+    for (i = 0; i < sec.length && i!=j; i++) {
+        sec[i].style.display = 'none';
+      }
+ }
+ sec1.onclick = function(){
+    change_display(0);
+ }
+ sec2.onclick = function(){
+    change_display(1);
+ }
+ sec3.onclick = function(){
+    change_display(2);
+ }
+ sec4.onclick = function(){
+    change_display(3);
+ }
