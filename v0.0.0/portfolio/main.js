7f54e2fe034dcf6e747a14b3e6674c0ffad27e62
KetanVerma <Ketan18710@gmail.com>
2019-01-22 08:13:14 +0530

    resending last nights working version

--- v0.0.0/portfolio/main.js
+++ v0.0.0/portfolio/main.js
@@ -5,24 +5,12 @@
  let sec = document.getElementsByClassName("content");
  function change_display(j)
  {
-<<<<<<< HEAD
   
     for (i = 0; i < sec.length; i++) {
         sec[i].style.display = 'none';
       }
       sec[j].style.display = 'block';
-=======
- 	// sec[0].style.display = 'none';
- 	// sec[1].style.display = 'none';
- 	// sec[2].style.display = 'none';
- 	// sec[3].style.display = 'none';
-  	// sec[j].style.display = 'flex';
-
-    for (i = 0; i < 4 && i!=j; i++) {
-        sec[i].style.display = 'none';
-      }
-
->>>>>>> a31763a5db413334c6c64f99630919555b7fdb61
+      sec[l].style.background = 'none';
  }
 
  sec1.onclick = function(){
@@ -37,33 +25,3 @@
  sec4.onclick = function(){
     change_display(3);
  }
-// function change_display(){
-// 	 if(sec1.onclick){
-// 		sec[0].style.display = 'flex';
-// 	 	sec[1].style.display = 'none';
-// 	 	sec[2].style.display = 'none';
-// 	 	sec[3].style.display = 'none';
-// 	 }
-	    
-// 	 if(sec2.onclick){
-// 		sec[0].style.display = 'none';
-// 	 	sec[1].style.display = 'flex';
-// 	 	sec[2].style.display = 'none';
-// 	 	sec[3].style.display = 'none';
-// 	 }
-// 	if(sec3.onclick){
-// 		sec[0].style.display = 'none';
-// 	 	sec[1].style.display = 'none';
-// 	 	sec[2].style.display = 'flex';
-// 	 	sec[3].style.display = 'none';
-// 	 }
-// 	if(sec4.onclick){
-// 		sec[0].style.display = 'none';
-// 	 	sec[1].style.display = 'none';
-// 	 	sec[2].style.display = 'none';
-// 	 	sec[3].style.display = 'flex';
-// 	 }
-
-// }
-
-change_display();
